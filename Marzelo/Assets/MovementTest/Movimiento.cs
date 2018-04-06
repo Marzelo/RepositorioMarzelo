244d59a03cc1ef3f6d7b04d45a26d72e532a1a45
Marzelo <marzelozorogastuan@gmail.com>
2018-04-05 22:01:02 -0500

    trasladar

--- Marzelo/Assets/MovementTest/Movimiento.cs
+++ Marzelo/Assets/MovementTest/Movimiento.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour {
+	
+	public float speed ;
+
 
 	// Use this for initialization
 	void Start () {
-
-        transform.position = new Vector3(1, 1, 0);
+		
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown (KeyCode.W) && transform.position.z ) {
+			transform.Translate (Vector3.forward *speed *Time.deltaTime);
+		}
+		if (Input.GetKeyDown (KeyCode.S) && transform.position.z) {
+			transform.Translate ( Vector3.back* speed*Time.deltaTime);
+		}
+		if (Input.GetKeyDown (KeyCode.A) && transform.position.x) {
+			transform.Translate ( Vector3.left * speed *Time.deltaTime);
+		}
+		if (Input.GetKeyDown (KeyCode.D) && transform.position.x) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
 	}
 }
