b64a7d6d988dfe563ecec9c8a5be346dc2de1635
marzelo <marzelozorogastuan@gmail.com>
2018-04-10 22:09:48 -0500

    primera parte

--- Marzelo/Assets/MovementTest/Movimiento.cs
+++ Marzelo/Assets/MovementTest/Movimiento.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour {
 	
 	public float speed ;
-
+	public Vector3 limit;
+	Rigidbody playerRB;
+	Vector3 inicialposition;
 
 	// Use this for initialization
 	void Start () {
-		
+		playerRB = GetComponent <Rigidbody> ();
+		inicialposition = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.W) && transform.position.z ) {
-			transform.Translate (Vector3.forward *speed *Time.deltaTime);
+		if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
+			playerRB.MovePosition(transform.position + ( Vector3.left * speed *Time.deltaTime));
 		}
-		if (Input.GetKeyDown (KeyCode.S) && transform.position.z) {
-			transform.Translate ( Vector3.back* speed*Time.deltaTime);
+		if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
+			playerRB.MovePosition(transform.position + ( Vector3.right * speed *Time.deltaTime));
 		}
-		if (Input.GetKeyDown (KeyCode.A) && transform.position.x) {
-			transform.Translate ( Vector3.left * speed *Time.deltaTime);
+		if (Input.GetKey (KeyCode.W) && transform.position.z < limit.z) {
+			playerRB.MovePosition(transform.position + ( Vector3.forward * speed *Time.deltaTime));
 		}
-		if (Input.GetKeyDown (KeyCode.D) && transform.position.x) {
-			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S) && transform.position.z > -limit.z) {
+			playerRB.MovePosition(transform.position + ( Vector3.back * speed *Time.deltaTime));
 		}
+
+	}
+	public void ResetPosition (){
+		transform.position = inicialposition;
+
 	}
 }
